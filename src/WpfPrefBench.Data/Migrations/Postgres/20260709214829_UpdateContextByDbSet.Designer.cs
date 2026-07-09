ba669a54e71cc83805dffcdc3f5b9f13a08237ec
alexander.yurtaev <ayurtaev@mail.ru>
2026-07-10 00:48:58 +0300

    Add migrations.

--- src/WpfPrefBench.Data/Migrations/Postgres/20260709214829_UpdateContextByDbSet.Designer.cs
+++ src/WpfPrefBench.Data/Migrations/Postgres/20260709214829_UpdateContextByDbSet.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WpfPrefBench.Data.DataContexts;
@@ -11,9 +12,11 @@ using WpfPrefBench.Data.DataContexts;
 namespace WpfPrefBench.Data.Migrations.Postgres
 {
     [DbContext(typeof(PostgresDataContext))]
-    partial class PostgresDataContextModelSnapshot : ModelSnapshot
+    [Migration("20260709214829_UpdateContextByDbSet")]
+    partial class UpdateContextByDbSet
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
