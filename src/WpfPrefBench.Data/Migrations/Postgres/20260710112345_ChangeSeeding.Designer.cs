f7517faf15b336dc4d15344b43ad0eb12c0cb144
alexander.yurtaev <ayurtaev@mail.ru>
2026-07-10 14:34:20 +0300

    Add ChangeSeeding migrations.

--- src/WpfPrefBench.Data/Migrations/Postgres/20260710112345_ChangeSeeding.Designer.cs
+++ src/WpfPrefBench.Data/Migrations/Postgres/20260710112345_ChangeSeeding.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WpfPrefBench.Data.DataContexts;
@@ -11,9 +12,11 @@ using WpfPrefBench.Data.DataContexts;
 namespace WpfPrefBench.Data.Migrations.Postgres
 {
     [DbContext(typeof(PostgresDataContext))]
-    partial class PostgresDataContextModelSnapshot : ModelSnapshot
+    [Migration("20260710112345_ChangeSeeding")]
+    partial class ChangeSeeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/WpfPrefBench.Data/Migrations/Postgres/20260710112345_ChangeSeeding.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WpfPrefBench.Data.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class ChangeSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Color", "IsActive", "Name", "ParentId" },
+                values: new object[,]
+                {
+                    { 1, "#000000", true, "Все заказы", null },
+                    { 2, "#333333", true, "По статусам", 1 },
+                    { 7, "#333333", true, "По зонам", 1 },
+                    { 3, "#4CAF50", true, "Новые", 2 },
+                    { 4, "#FF9800", true, "В пути", 2 },
+                    { 5, "#2196F3", true, "Доставлены", 2 },
+                    { 6, "#F44336", true, "Отменены", 2 },
+                    { 8, "#E91E63", true, "Центр", 7 },
+                    { 9, "#00BCD4", true, "Север", 7 },
+                    { 10, "#88C34A", true, "Юг", 7 },
+                    { 11, "#9C27B0", true, "Запад", 7 },
+                    { 12, "#FF5722", true, "Восток", 7 }
+                });
+        }
+    }
+}
