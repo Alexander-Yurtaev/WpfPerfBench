ead225a27f36e358bb0f6982e20c7f6c264ec48e
alexander.yurtaev <ayurtaev@mail.ru>
2026-07-24 22:06:26 +0300

    Add base for BusyIndicator.

---
+++ src/WpfPerfBench/Controls/BusyIndicator.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WpfPerfBench.Controls
+{
+    /// <summary>
+    /// Interaction logic for BusyIndicator.xaml
+    /// </summary>
+    public partial class BusyIndicator : UserControl
+    {
+        public BusyIndicator()
+        {
+            InitializeComponent();
+        }
+    }
+}
