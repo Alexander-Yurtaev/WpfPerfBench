6e2aafe4650a8b712e65d9b7271cc043a8a21cb8
alexander.yurtaev <ayurtaev@mail.ru>
2026-08-02 02:05:05 +0300

    Fix ModalDialog.

---
+++ src/WpfPerfBench/DesignViewModels/DesignSuccessDialog.cs
@@ -0,0 +1,11 @@
+﻿using WpfPerfBench.ViewModels.Dialogs;
+
+namespace WpfPerfBench.DesignViewModels;
+
+public class DesignSuccessDialog : SuccessDialog
+{
+    public DesignSuccessDialog() 
+        : base("Design Success Dialog", "DesignSuccessDialog, DesignSuccessDialog, DesignSuccessDialog")
+    {
+    }
+}
